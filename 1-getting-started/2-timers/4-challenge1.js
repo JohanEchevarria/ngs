9e55fc9b6b9a78acb4c9ecfb5ace2ea26e365962
JohanEchevarria <johan.echevarriap@gmail.com>
2019-08-31 12:43:34 -0500

    Solved challenge timer - challenge 01

--- 1-getting-started/2-timers/4-challenge1.js
+++ 1-getting-started/2-timers/4-challenge1.js
@@ -1,6 +1,13 @@
-const theOneFunc = () => {};
+const theOneFunc = () => {
+    console.log('Hello after 4 seconds');
+};
+
+const theOne = () => {
+    console.log('Hello after 8 seconds');
+};
 
 setTimeout(theOneFunc, 4 * 1000);
+setTimeout(theOne, 8 * 1000);
 
 // Hello after 4 seconds
 
