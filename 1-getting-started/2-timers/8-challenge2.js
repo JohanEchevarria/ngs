6b82d6f7fbac730382cc10004ee0cfa0f9ca1d7e
JohanEchevarria <johan.echevarriap@gmail.com>
2019-09-06 15:25:01 -0500

    solved

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,13 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+let contador = 0;
+const intervalo = setInterval(() => {
+  console.log('Hello World');
+  contador += 1;
+
+  if (contador === 5) {
+    console.log('Done');
+    clearInterval(intervalo);
+  }
+}, 1000);
